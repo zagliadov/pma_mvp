789d2da79da604b340fb0437e5b9bc47f503fed8
Daniil <portal.sid.grand2@gmail.com>
2023-02-23 10:57:33 +0200

    Base search input

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,29 +1,20 @@
 import { FC, useState } from "react";
-import { FaOpenMenu, FaCloseMenu, FaSearch } from "../icons/icons";
+import { FaOpenMenu, FaCloseMenu } from "../icons/icons";
+import { SearchInput } from "./SearchInput/SearchInput";
 
 export const Navigation: FC = () => {
   const [open, setOpen] = useState<boolean>(false);
   return (
     <div className="flex justify-between p-2">
-      <button onClick={() => setOpen(!open)}>
-        {open ? <FaCloseMenu /> : <FaOpenMenu />}
-      </button>
-
-      <div className="relative">
-        <span className="absolute inset-y-0 left-0 flex items-center px-2.5">
-          <button
-            type="submit"
-            className="focus:outline-none focus:shadow-outline"
-          >
-            <FaSearch />
-          </button>
-        </span>
-        <input
-          type="search"
-          className="py-2 border border-gray-100 text-gray-600 text-sm rounded pl-8 focus:outline-none"
-          placeholder="Search..."
-        />
+      <div className="border w-1/5">
+        <button onClick={() => setOpen(!open)}>
+          {open ? <FaCloseMenu /> : <FaOpenMenu />}
+        </button>
       </div>
+
+      <SearchInput />
+
+      <div className="w-1/5 border"></div>
     </div>
   );
 };
---
+++ src/components/Navigation/SearchInput/SearchInput.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+import { FaSearch } from "../../icons/icons";
+
+export const SearchInput: FC = () => {
+  return (
+    <div className="flex w-3/5 justify-end items-center">
+      <button
+        type="submit"
+        disabled
+        className="px-2.5 z-10 mr-[-35px] focus:outline-none focus:shadow-outline"
+      >
+        <FaSearch />
+      </button>
+      <input
+        type="search"
+        className="py-2 border border-gray-100 focus:w-full text-gray-600 text-sm rounded pl-8 focus:outline-none"
+        placeholder="Search..."
+      />
+    </div>
+  );
+};
