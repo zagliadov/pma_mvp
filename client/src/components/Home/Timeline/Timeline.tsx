31405a7f439570b8b85e9c3944a76dbceda29ae5
Daniil <portal.sid.grand2@gmail.com>
2023-03-24 15:51:19 +0200

    timeline tasks

--- client/src/components/Home/Timeline/Timeline.tsx
+++ client/src/components/Home/Timeline/Timeline.tsx
@@ -8,27 +8,43 @@ export const Timeline: FC = () => {
     tasks: tasks.tasks,
   }));
 
-  const daysInMonth = moment().daysInMonth(); // количество дней в текущем месяце
-  const currentDate = moment().startOf("month"); // начальная дата текущего месяца
-  const daysOfMonth = [];
+  const daysInMonth: any = moment().daysInMonth(); // количество дней в текущем месяце
+  const currentDate: any = moment().startOf("month"); // начальная дата текущего месяца
+  const daysOfMonth: any = [];
+  const tasksByDate: any = {}; // tasks grouped by date
+  tasks.forEach((task: any) => {
+    const taskStartDate = moment(task.task_goal_start);
+    const taskEndDate = moment(task.task_goal_end);
+    const numOfDays = taskEndDate.diff(taskStartDate, "days");
+    for (let i = 0; i <= numOfDays; i++) {
+      const dateKey: any = moment(task.task_goal_start).add(i, "days").format("D MMM");
+      if (!tasksByDate[dateKey]) {
+        tasksByDate[dateKey] = [task];
+      } else {
+        tasksByDate[dateKey].push(task);
+      }
+    }
+  });
   for (let i = 0; i < daysInMonth; i++) {
-    daysOfMonth.push(currentDate.clone().add(i, "days").format("D MMM"));
+    const date = currentDate.clone().add(i, "days").format("D MMM");
+    daysOfMonth.push(date);
   }
 
-  const taskList = daysOfMonth.map((date) => {
-    const tasksForDate = tasks.filter((task: any) => {
-      const taskStartDate = moment(task.task_goal_start);
-      const taskEndDate = moment(task.task_goal_end);
-      const dateMoment = moment(date, "D MMM");
-      return dateMoment.isBetween(taskStartDate, taskEndDate, null, "[]");
-    });
+  const taskList = daysOfMonth.map((date: any) => {
+    const tasksForDate = tasksByDate[date] || [];
+    const tasksForDate2 = tasksForDate.splice(Math.ceil(tasksForDate.length / 2));
     return (
       <tr key={date}>
         <td key={date} className="flex flex-col">
           <span>{date}</span>
         </td>
         <td>
-          {tasksForDate.map((task) => (
+          {tasksForDate.map((task: any) => (
+            <span key={task.id}>{task.name}</span>
+          ))}
+        </td>
+        <td>
+          {tasksForDate2.map((task: any) => (
             <span key={task.id}>{task.name}</span>
           ))}
         </td>
@@ -41,31 +57,11 @@ export const Timeline: FC = () => {
       <thead>
         <tr>
           <th>date</th>
-          <th>tasks</th>
+          <th>tasks 1</th>
+          <th>tasks 2</th>
         </tr>
       </thead>
-      <tbody>
-        {/* <tr className="border"> */}
-        {taskList}
-        {/* </tr> */}
-      </tbody>
+      <tbody>{taskList}</tbody>
     </table>
-    // <table>
-    //   <thead>
-    //     <tr>
-    //       <th>date</th>
-    //       <th>tasks</th>
-    //     </tr>
-    //   </thead>
-    //   <tbody>
-    //     <tr>
-    //       {daysOfMonth.map((date) => (
-    //         <td key={date} className="flex flex-col">
-    //           <span>{date}</span>
-    //         </td>
-    //       ))}
-    //     </tr>
-    //   </tbody>
-    // </table>
   );
 };
